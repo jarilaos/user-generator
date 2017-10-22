61a92a9f07a30fdef35f4000b93eabc6c5bde7f6
jarilaos <32414258+jarilaos@users.noreply.github.com>
2017-10-23 00:08:56 +0200

    more customization options added

--- user-generator.py
+++ user-generator.py
@@ -6,114 +6,233 @@
 """
 import argparse
 import string
+import sys
+import os
 
-parser = argparse.ArgumentParser(description='Generate usernames with different alphanumeric combinations.')
-#Files
-parser.add_argument("Names", help="The path to the file containing the dictionary with names.")
-parser.add_argument("Surnames", help="The path to the file containing the dictionary with surnames.")
-parser.add_argument("-o", "--output", help="If you want to specify the name of the output file. Default is usernames", default="usernames.txt")
-parser.add_argument("-ef", "--emailsfile", help="If you want to specify the name of the output file for emails. Default is emails", default="emails.txt")
-parser.add_argument("-lf", "--leetfile", help="If you want to specify the name of the output file for leet format usernames. Default is leet", default="leet.txt")
-# Options
-parser.add_argument("-mC", "--min-chars", help="The minimum number of chars from the username. Default is 1.", type=int, default=1)
-parser.add_argument("-xC", "--max-chars", help="The maximum number of chars from the Name. Default is 20.", type=int, default=25)
-parser.add_argument("-mY", "--min-year", help="The year it starts. Default is 1940.", type=int, default=1940)
-parser.add_argument("-xY", "--max-year", help="The year it ends. Default is 2018.", type=int, default=2018)
-parser.add_argument("-m", "--mode", help="If you want the results to be appended to the file or to overwrite. Default is overWrite.", choices=["a", "w"], default="w")
-parser.add_argument("-u", "--union", help="Select if you want a binding character [_ . - ] etc. Default is None. ", nargs='+', default="")
-parser.add_argument("-e", "--email", help="Select if you want an email [gmail.com hotmail.us] etc. Default is None. ", nargs='+', default="")
-parser.add_argument("-l", "--leet", help="If you want, select the level to convert to leet format [username -> us3rn4m3]. Level 1 only vowels, level 2 some consonants too. Default is None. ", type=int, choices=[1, 2], default=0)
-results = parser.parse_args()
+# Console Colors
+G = '\033[92m'  # green
+Y = '\033[93m'  # yellow
+B = '\033[94m'  # blue
+R = '\033[91m'  # red
+W = '\033[0m'   # white
 
-def leet(text):
-    getchar = lambda c: chars[c] if c in chars else c
+#You should not be using windows for that XD
+if sys.platform.startswith('win'):
+    try:
+        import win_unicode_console , colorama
+        win_unicode_console.enable()
+        colorama.init()
+    except:
+        print("[!] Error: Coloring libraries not installed. If you want to do it, type this: [python.exe -m pip install win_unicode_console colorama]")
+        G = Y = B = R = W = ''
+#username counter
+n=0
+default_surnames = "Top_World_Surnames_ByDefault.txt"
+def banner():
+    print('''%s
+                             __    __       _______  _______  ______      
+                            |  |  |  |     /       ||   ____||   _  \     
+                            |  |  |  |    |   (----`|  |__   |  |_)  |    
+                            |  |  |  |     \   \    |   __|  |      /     
+                            |  '--'  | .----)   |   |  |____ |  |\  \----.
+                             \______/  |_______/    |_______||__| `._____|
+       _______  _______  __   __   _______  ______          ___    ___________   ______    ______      
+      /  _____||   ____||  \ |  | |   ____||   _  \        /   \  |           | /  __  \  |   _  \     
+     |  |  __  |  |__   |   \|  | |  |__   |  |_)  |      /  ^  \ `---|  |----`|  |  |  | |  |_)  |    
+     |  | |_ | |   __|  |  . `  | |   __|  |      /      /  /_\  \    |  |     |  |  |  | |      /     
+     |  |__| | |  |____ |  |\   | |  |____ |  |\  \----./  _____  \   |  |     |  `--'  | |  |\  \----.
+      \______| |_______||__| \__| |_______||__| `._____/__/     \__\  |__|      \______/  |__| `._____|%s
+      '''% (Y,W))
+
+
+def parser_error(errmsg):
+    banner()
+    print("Usage: python " + sys.argv[0] + " [Options] use -h for help")
+    print(R + "Error: " + errmsg + W)
+    sys.exit()
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Generate usernames with different alphanumeric combinations.')
+    parser.error = parser_error
+    parser._positionals.title = "FILES REQUIRED"
+    parser._optionals.title = "OPTIONS"
+    #Files
+    parser.add_argument("Names", help="The path of the file containing the dictionary with names.")
+    parser.add_argument("-s", "--surnames", help="The path of the file containing the dictionary with surnames. If none is specified, the 16 most common world surnames will be used")
+    parser.add_argument("-o", "--output", help="If you want to specify the name of the output file. Default is usernames.txt", default="usernames.txt")
+    parser.add_argument("-ef", "--emailsfile", help="If you want to specify the name of the output file for emails. Default is emails.txt", default="emails.txt")
+    parser.add_argument("-lf", "--leetfile", help="If you want to specify the output file name for usernames in leet format. Default is leet", default="leet.txt")
+    # Options
+    parser.add_argument("-mC", "--min-chars", help="The minimum number of chars from the username. Default is 1.", type=int, default=1)
+    parser.add_argument("-xC", "--max-chars", help="The maximum number of chars from the Name. Default is 25.", type=int, default=25)
+    parser.add_argument("-mY", "--min-year", help="The year it starts. Default is 1942...No country for old men)", type=int, default=1942)
+    parser.add_argument("-xY", "--max-year", help="The year it ends. Default is 2018.", type=int, default=2018)
+    parser.add_argument("-n", "--number", help="Add a especific number. Default is None.", type=int, nargs='+')
+    parser.add_argument("-u", "--union", help="Select if you want a binding character [_ . - ] etc. Default is None. ", nargs='+', default="")
+    parser.add_argument("-e", "--email", help="Select if you want email usernames [gmail.com hotmail.us] etc. Default is None. ", nargs='+')
+    parser.add_argument("-l", "--leet", help="To convert to leet format [username -> us3rn4m3]. 1:[e, o] 2:[a, e, i, o] 3:[a, e, i, o, s, t] 4:[a, e, i, o, s, t, l, b, g]. Default is None. ", type=int, choices=[1, 2, 3, 4])
+    parser.add_argument("-m", "--mode", help="If you want the results to be appended to the file or to overwrite. Default is overWrite.", choices=["a", "w"], default="w")
+    parser.add_argument("-d", "--delduplicates", action='store_true', help="Sometimes the surnames are used as names and vice versa and this generates duplicates.")
+    return parser.parse_args()
+
+def create_surnames_file():
+    results.surnames = default_surnames
+    with open(results.surnames, 'w') as outfile:
+        data = ['smith', 'garcia', 'gonzalez', 'hernandez', 'smirnov', 'müller', 'lee', 'li', 'zhang', 'wang', 'chang', 'nguyen', 'kumar', 'martin', 'silva', 'rossi', 'johnson']
+        for line in data:
+            outfile.write("%s\n"%line)
+
+def leet(line):
     if results.leet == 1:
-        chars = {"a":"4","e":"3","i":"1","o":"0","A":"4","E":"3","I":"1","O":"0"}
-    else:
-        chars = {"a":"4","e":"3","i":"1","o":"0","A":"4","E":"3","I":"1","O":"0","s":"5","S":"5","l":"1","L":"1","t":"7","T":"7","b":"8","B":"8"}
-    return ''.join(getchar(c) for c in text)
+        chars = {"e":"3","E":"3","o":"0","O":"0"}
+    elif results.leet == 2:
+        chars = {"a":"4","A":"4","e":"3","E":"3","i":"1","I":"1","o":"0","O":"0"}
+    elif results.leet == 3:
+        chars = {"a":"4","A":"4","e":"3","E":"3","i":"1","I":"1","o":"0","O":"0","s":"5","S":"5","t":"7","T":"7"}    
+    elif results.leet == 4:
+        chars = {"a":"4","A":"4","e":"3","E":"3","i":"1","I":"1","o":"0","O":"0","s":"5","S":"5","t":"7","T":"7","l":"1","L":"1","b":"8","B":"8","g":"6","G":"6"}
+    # TODO:customize the letters to convert
+    getchar = lambda c: chars[c] if c in chars else c
+    return ''.join(getchar(c) for c in line)
 
-print('''
-                         __    __       _______  _______  ______      
-                        |  |  |  |     /       ||   ____||   _  \     
-                        |  |  |  |    |   (----´|  |__   |  |_)  |    
-                        |  |  |  |     \   \    |   __|  |      /     
-                        |  '--'  | .----)   |   |  |____ |  |\  \----.
-                         \______/  |_______/    |_______||__| `._____|
-   _______  _______  __   __   _______  ______          ___    ___________   ______    ______      
-  /  _____||   ____||  \ |  | |   ____||   _  \        /   \  |           | /  __  \  |   _  \     
- |  |  __  |  |__   |   \|  | |  |__   |  |_)  |      /  ^  \ `---|  |----´|  |  |  | |  |_)  |    
- |  | |_ | |   __|  |  . `  | |   __|  |      /      /  /_\  \    |  |     |  |  |  | |      /     
- |  |__| | |  |____ |  |\   | |  |____ |  |\  \----./  _____  \   |  |     |  `--'  | |  |\  \----.
-  \______| |_______||__| \__| |_______||__| `._____/__/     \__\  |__|      \______/  |__| `._____|
-  
-\nThe file is being generated with the following options...''')
-print(results)
-
-with open(results.output, results.mode.lower()) as outfile:
-    with open(results.Surnames, 'r') as reader:
-        data = reader.read().splitlines() 
-        for line in data:
-            if len(line) >= results.min_chars and len(line) <= results.max_chars:
-                outfile.write("%s\n"%line)
-            if len(line) >= results.min_chars-1 and len(line) <= results.max_chars-1:
-                for letter in string.ascii_lowercase:
-                    outfile.write("%s%s\n" % (letter, line))
-            if len(line) >= results.min_chars-2 and len(line) <= results.max_chars-2:
-                for union in results.union:
-                    for x in string.ascii_lowercase:
-                        outfile.write("%s%s%s\n" % (x,union, line))
-
-    with open(results.Names, 'r') as names:
-        data = names.read().splitlines() 
-        for name in data:
-            if len(name) >= results.min_chars and len(name) <= results.max_chars:
-                outfile.write("%s\n"%name)
-            if len(name) >= results.min_chars-1 and len(name) <= results.max_chars-1:
-                for i in range(10):
-                    outfile.write("%s%s\n" % (name, i))
-            if len(name) >= results.min_chars-2 and len(name) <= results.max_chars-2:
-                for i in range(100):
-                    outfile.write("%s%s\n" % (name, '{:02d}'.format(i)))
-            if len(name) >= results.min_chars-3 and len(name) <= results.max_chars-3:
-                outfile.write("%s%d\n" % (name, 123))
+def add_numbers(line):
+    global n
+    l = len(line)
+    #Line+Number
+    if results.number is not None:
+        for num in results.number:
+            if l+len(str(num)) >= results.min_chars and l+len(str(num)) <= results.max_chars:
+                outfile.write("%s%s\n" % (line, num))
+                n+=1
+    if l+1 >= results.min_chars and l+1 <= results.max_chars:
+        for i in range(10):
+            outfile.write("%s%s\n" % (line, i))
+            n+=1
+    if l+2 >= results.min_chars and l+2 <= results.max_chars:
+        for i in range(100):
+            outfile.write("%s%s\n" % (line, '{:02d}'.format(i)))
+            n+=1
+    if l+3 >= results.min_chars and l+3 <= results.max_chars:
+        outfile.write("%s%d\n" % (line, 123))
+        n+=1
+    if l+4 >= results.min_chars and l+4 <= results.max_chars:
+        for i in range(results.min_year, results.max_year+1):
+            outfile.write("%s%d\n" % (line, i))
+            n+=1
+    #Line+Union+Number        
+    for union in results.union:
+        if results.number is not None:
+            for num in results.number:
+                if l+len(str(num))+1 >= results.min_chars and l+len(str(num))+1 <= results.max_chars:
+                    outfile.write("%s%s%d\n" % (line,union, num))
+                    n+=1
+        if l+2 >= results.min_chars and l+2 <= results.max_chars:
+            for i in range(10):
+                outfile.write("%s%s%d\n" % (line, union, i))
+                n+=1
+        if l+3 >= results.min_chars and l+3 <= results.max_chars:
+            for i in range(100):
+                outfile.write("%s%s%s\n" % (line, union, '{:02d}'.format(i)))
+                n+=1
+        if l+4 >= results.min_chars and l+4 <= results.max_chars:
+            outfile.write("%s%s%d\n" % (line, union, 123))
+            n+=1
+        if l+5 >= results.min_chars and l+5 <= results.max_chars:
             for i in range(results.min_year, results.max_year+1):
-                if len(name) >= results.min_chars-4 and len(name) <= results.max_chars-4:
-                    outfile.write("%s%d\n" % (name, i))
-                    
-            for union in results.union:
-                if len(name) >= results.min_chars-2 and len(name) <= results.max_chars-2:
-                    for i in range(10):
-                        outfile.write("%s%s%d\n" % (name, union, i))
-                if len(name) >= results.min_chars-3 and len(name) <= results.max_chars-3:
-                    for i in range(100):
-                        outfile.write("%s%s%s\n" % (name, union, '{:02d}'.format(i)))
-                if len(name) >= results.min_chars-4 and len(name) <= results.max_chars-4:
-                    outfile.write("%s%s%d\n" % (name, union, 123))
-                for i in range(results.min_year, results.max_year+1):
-                    if len(name) >= results.min_chars-5 and len(name) <= results.max_chars-5:
-                        outfile.write("%s%s%d\n" % (name, union, i))   
+                outfile.write("%s%s%d\n" % (line, union, i))
+                n+=1
 
-    with open(results.Surnames, 'r') as reader , open(results.Names, 'r') as reader2:
-        surnames = reader.read().splitlines()
-        names = reader2.read().splitlines()
-        for line in surnames:
-            for name in names:
-                if len(name+line) >= results.min_chars and len(name+line) <= results.max_chars:
-                    outfile.write("%s%s\n" % (name, line))
-                for union in results.union:
-                    if len(name+line) >= results.min_chars-1 and len(name+line) <= results.max_chars-1:
-                        outfile.write("%s%s%s\n" % (name, union, line))
 
-if results.leet != 0:
-    with open(results.output, 'r') as reader, open (results.leetfile, results.mode.lower()) as outfile:
-        data = reader.read().splitlines() 
-        for line in data:
-            outfile.write("%s\n" % (leet(line)))   
+if __name__ == "__main__":
+    results = parse_args()
+    banner()
 
-if results.email != "":
-    with open(results.output, 'r') as reader, open (results.emailsfile, results.mode.lower()) as outfile:
-        data = reader.read().splitlines() 
-        for e in results.email:
+    if results.surnames is None:
+        create_surnames_file()      
+
+    with open(results.output, results.mode.lower()) as outfile:
+        with open(results.surnames, 'r') as reader:
+            data = reader.read().splitlines() 
             for line in data:
-                outfile.write("%s@%s\n" % (line, e))
+                if len(line) >= results.min_chars and len(line) <= results.max_chars:
+                    outfile.write("%s\n"%line)
+                    n+=1
+                #Letter+Surname    
+                if len(line) >= results.min_chars-1 and len(line) <= results.max_chars-1:
+                    for letter in string.ascii_lowercase:
+                        outfile.write("%s%s\n" % (letter,line))
+                        n+=1
+                #Letter+Union+Surname 
+                if len(line) >= results.min_chars-2 and len(line) <= results.max_chars-2:
+                    for union in results.union:
+                        for x in string.ascii_lowercase:
+                            outfile.write("%s%s%s\n" % (x,union,line))
+                            n+=1
+                add_numbers(line)
+        
+        with open(results.Names, 'r') as names:
+            data = names.read().splitlines() 
+            for name in data:
+                if len(name) >= results.min_chars and len(name) <= results.max_chars:
+                    outfile.write("%s\n"%name)
+                    n+=1
+                #Name+Letter  
+                if len(name) >= results.min_chars-1 and len(name) <= results.max_chars-1:
+                    for letter in string.ascii_lowercase:
+                        outfile.write("%s%s\n" % (name, letter))
+                        n+=1
+                #Name+Union+Letter 
+                if len(name) >= results.min_chars-2 and len(name) <= results.max_chars-2:
+                    for union in results.union:
+                        for x in string.ascii_lowercase:
+                            outfile.write("%s%s%s\n" % (name,union,x))
+                            n+=1
+                add_numbers(name)  
+
+        with open(results.surnames, 'r') as reader , open(results.Names, 'r') as reader2:
+            surnames = reader.read().splitlines()
+            names = reader2.read().splitlines()
+            for line in surnames:
+                for name in names:
+                    if len(name+line) >= results.min_chars and len(name+line) <= results.max_chars:
+                        outfile.write("%s%s\n" % (name, line))
+                        n+=1
+                    if len(name+line) >= results.min_chars-1 and len(name+line) <= results.max_chars-1:
+                        for union in results.union:
+                            outfile.write("%s%s%s\n" % (name, union, line))
+                            n+=1
+
+        print ("Usernames generated:"+G+str(n))
+        print (W+"Usernames dictionary: "+G+results.output+W)
+
+    if results.surnames == default_surnames:
+        os.remove(results.surnames)
+
+    if results.delduplicates:
+        n=0
+        lines_stored = set()
+        with open("temp", "w") as outfile:
+            with open(results.output, "r") as reader:
+                for line in reader:
+                    if line not in lines_stored:
+                        outfile.write(line)
+                        lines_stored.add(line)
+                        n+=1
+        os.rename("temp", results.output) 
+        print ("Usernames after delete duplicates: "+G+str(n)+W)
+
+    if results.leet is not None:
+        with open(results.output, 'r') as reader, open (results.leetfile, results.mode.lower()) as outfile:
+            data = reader.read().splitlines() 
+            for line in data:
+                outfile.write("%s\n" % (leet(line)))
+        print ("Leet dictionary: "+G+results.leetfile+W)   
+
+    if results.email is not None:
+        with open(results.output, 'r') as reader, open (results.emailsfile, results.mode.lower()) as outfile:
+            data = reader.read().splitlines() 
+            for e in results.email:
+                for line in data:
+                    outfile.write("%s@%s\n" % (line, e))
+        print ("Emails dictionary: "+G+results.emailsfile+W)
+
